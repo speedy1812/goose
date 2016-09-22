86e438a3aed3e034a607e3f455ef3015e04d4723
Nathan Day <adayinukraine@gmail.com>
2016-09-22 17:44:31 +0300

    Make assets fancy w/ Sass, Bourbon, Neat, jquery

--- Gemfile
+++ Gemfile
@@ -1,4 +1,11 @@
 source 'https://rubygems.org'
 
+source "https://rails-assets.org" do
+  gem "rails-assets-jquery"
+  gem "rails-assets-moment"
+end
+
+gem "bourbon"
+gem "neat"
 gem 'middleman', '>= 4.0.0'
 gem 'middleman-livereload'
--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,7 @@
-// This is where it all goes :)
+//= require jquery
+//= require moment
+
+$(function() {
+	var today = moment().format("dddd MMM Do, YYYY");
+	$('footer').html('<p>Hello from jQuery land. Today is ' + today + '</p>');
+});
